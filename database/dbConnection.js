97a26b83d8a22bdaa195c000ee228c65dad536f1
Sweety <138968259+GitSweety-21@users.noreply.github.com>
2024-05-31 14:28:31 +0530

    database

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = ()=>{
     mongoose.connect(process.env.MONGO_URI,{
-        dbName:"RESTURANT"
+        dbName:"RESTURANT1"
     }).then(()=>{
         console.log("Connected to database sucessfully");
     }).catch(err=>{
